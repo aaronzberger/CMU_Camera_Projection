084bc27bfe39161e31160acd98ee8b020cea9239
Aaron Berger <aaronzberger@gmail.com>
2021-07-14 16:44:26 -0400

    Cleanup and comment

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@
 
 # -*- encoding: utf-8 -*-
 
-from numpy.lib.function_base import flip
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
@@ -18,7 +17,6 @@ from CMU_Mask_R_CNN import msg
 from CMU_Camera_Projection.msg import predictions
 from vision_msgs.msg import BoundingBox3D
 from message_filters import ApproximateTimeSynchronizer
-from scipy.spatial.transform import Rotation as R
 
 
 def point_cloud(points, parent_frame):
@@ -76,14 +74,12 @@ class Camera_Projection_Node:
             [self.sub_velodyne, self.sub_predictions], queue_size=20, slop=0.2)
         ts.registerCallback(self.projection_callback)
 
-        self.pub_viz = rospy.Publisher(
+        self.pub_projection = rospy.Publisher(
             '/projection_viz', Image, queue_size=1)
         self.pub_predictions = rospy.Publisher(
             '/projected_predictions', predictions, queue_size=1)
-        self.pub_test = rospy.Publisher(
-            'vis_full', Image, queue_size=1)
-        self.pub_cloud = rospy.Publisher(
-            '/transformed', PointCloud2, queue_size=1)
+        self.pub_object_depths = rospy.Publisher(
+            '/vis_full', Image, queue_size=1)
 
     def projection_callback(self, data_velo, data_predictions):
         image = cv2.cvtColor(self.cv_bridge.imgmsg_to_cv2(
@@ -109,12 +105,6 @@ class Camera_Projection_Node:
         # Transpose for easier matrix multiplication
         lidar = lidar.transpose()
 
-        # If you wish to publish the transformed pointcloud for testing
-        pts_3d = self.transforms.lidar_to_cam @ lidar
-        pts_3d = pts_3d.transpose().astype('float32')
-        pts_3d[:, 3] = pts_3d[:, 2]
-        self.pub_cloud.publish(point_cloud(pts_3d, 'map'))
-
         # Perform the actual transformation to camera frame
         transformed = proj_lidar_to_cam @ lidar
         transformed[:2, :] /= transformed[2, :]
@@ -128,38 +118,41 @@ class Camera_Projection_Node:
         # Get image pixels where there are lidar points
         imgfov_lidar_pixels = transformed[:, inds]
         imgfov_lidar_pixels[:2, :] = np.around(imgfov_lidar_pixels[:2, :]).astype(np.float32)
-        # rotation = R.from_euler(seq='x', angles=np.pi / 2)
-        # imgfov_lidar_pixels = rotation.apply(imgfov_lidar_pixels.transpose())
-        # imgfov_lidar_pixels = imgfov_lidar_pixels.transpose()
+
+        # Temporary, to fix the extrinsic calibration
+        # TODO: Re-calibrate extrinsic transformation
         imgfov_lidar_pixels[0, :] -= 25
         for i in range(len(imgfov_lidar_pixels[0, :])):
             imgfov_lidar_pixels[0, i] = max(0, imgfov_lidar_pixels[0, i])
 
         # Visualization: plot lidar points on the image colored by depth
+        cmap = plt.cm.get_cmap('hsv', 256)
+        cmap = np.array([cmap(i) for i in range(256)])[:, :3] * 255
+        max_depth = np.amax(imgfov_lidar_pixels[2, :])
 
-        img = self.render_on_image(imgfov_lidar_pixels, image)
-        ros_msg = self.cv_bridge.cv2_to_imgmsg(img, encoding="bgr8")
-        self.pub_viz.publish(ros_msg)
+        for i in range(imgfov_lidar_pixels.shape[1]):
+            depth = imgfov_lidar_pixels[2, i]
+            color = cmap[int(depth * 255.0 / max_depth), :]
+            cv2.circle(image, (int(imgfov_lidar_pixels[0, i]), int(imgfov_lidar_pixels[1, i])),
+                       2, color=tuple(color), thickness=-1)
 
-        # Get the original lidar points that correspond with the pixel points
-        # (usesful for other operations, but will be unused for visualization)
-        # lidar_points_in_imgfov = pts_velo.transpose()[inds, :]
+        ros_msg = self.cv_bridge.cv2_to_imgmsg(image, encoding="bgr8")
+        self.pub_projection.publish(ros_msg)
 
         # Calculate depths of the objects
         object_depths = []
 
         masks = [self.cv_bridge.imgmsg_to_cv2(i) for i in data_predictions.masks]
-        # print(max(imgfov_lidar_pixels[0]), max(imgfov_lidar_pixels[1]))
         for mask in masks:
-            # print(mask.shape)
             mask_depths = []
 
             for pixel in imgfov_lidar_pixels.transpose():
-                # print(pixel, int(1032 - pixel[1]), int(1384 - pixel[0]))
-
                 if mask[int(1032 - pixel[1] - 1), int(1384 - pixel[0]) - 1]:
-                    # print(pixel, (1384 - pixel[0], 1032 - pixel[1]))
                     mask_depths.append(pixel[2])
+                    depth = pixel[2]
+                    color = cmap[int(depth * 255.0 / max_depth), :]
+                    corners = ((int(pixel[0] + 4), int(pixel[1] + 4)), (int(pixel[0] - 4), int(pixel[1] - 4)))
+                    cv2.rectangle(image, corners[0], corners[1], color=tuple(color), thickness=-1)
 
             if len(mask_depths) == 0:
                 object_depths.append(-1)
@@ -175,19 +168,18 @@ class Camera_Projection_Node:
 
         self.pub_predictions.publish(pub_msg)
 
-        img_boxes = np.copy(img)
-        img_boxes = cv2.flip(img_boxes, flipCode=-1)
+        # Visualization: plot lidar points on image colored by depth and draw
+        # predicted bounding boxes and estimated depth (distance from drone)
+        img_boxes = cv2.flip(np.copy(image), flipCode=-1)
         for box, depth in zip(data_predictions.bboxes, object_depths):
             r_x, r_y = box.size_x / 2, box.size_y / 2
             c_x, c_y = box.center.x, box.center.y
-
             x = [c_x + r_x, c_x + r_x, c_x - r_x, c_x - r_x]
             y = [c_y + r_y, c_y - r_y, c_y + r_y, c_y - r_y]
+
             corners = np.empty((4, 2), dtype=np.int)
             corners[:, 0] = x
             corners[:, 1] = y
-
-            text = corners[0][0], corners[0][1] + r_y
 
             img_boxes = cv2.line(img_boxes, corners[0], corners[1], color=(204, 255, 255), thickness=2, lineType=cv2.LINE_AA)
             img_boxes = cv2.line(img_boxes, corners[0], corners[2], color=(204, 255, 255), thickness=2, lineType=cv2.LINE_AA)
@@ -195,23 +187,7 @@ class Camera_Projection_Node:
             img_boxes = cv2.line(img_boxes, corners[2], corners[3], color=(204, 255, 255), thickness=2, lineType=cv2.LINE_AA)
             img_boxes = cv2.putText(img_boxes, '{}m'.format(int(depth) if depth != -1 else '?'), (corners[0][0], corners[0][1] - int(r_y)), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(204, 255, 255), thickness=2)
 
-        self.pub_test.publish(self.cv_bridge.cv2_to_imgmsg(cv2.flip(img_boxes, flipCode=-1), encoding="bgr8"))
-
-        # print(object_depths)
-
-    def render_on_image(self, lidar_pixels, img):
-        # img = cv2.flip(img, flipCode=0)
-        cmap = plt.cm.get_cmap('hsv', 256)
-        cmap = np.array([cmap(i) for i in range(256)])[:, :3] * 255
-        max_depth = np.amax(lidar_pixels[2, :])
-
-        for i in range(lidar_pixels.shape[1]):
-            depth = lidar_pixels[2, i]
-            color = cmap[int(depth * 255.0 / max_depth), :]
-            cv2.circle(img, (int(lidar_pixels[0, i]), int(lidar_pixels[1, i])),
-                       2, color=tuple(color), thickness=-1)
-
-        return img
+        self.pub_object_depths.publish(self.cv_bridge.cv2_to_imgmsg(cv2.flip(img_boxes, flipCode=-1), encoding="bgr8"))
 
 
 if __name__ == '__main__':
